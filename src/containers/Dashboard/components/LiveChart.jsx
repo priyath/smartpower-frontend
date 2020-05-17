3ee3c1af022af72117c18aa9113b95c628d74d95
Priyath Gregory <priyath.gregory@syscolabs.com>
2020-05-17 09:04:31 +0530

    changed point interval for live chart

--- src/containers/Dashboard/components/LiveChart.jsx
+++ src/containers/Dashboard/components/LiveChart.jsx
@@ -64,7 +64,7 @@ export default class LiveChart extends React.Component {
                     data: [
                     ],
                     pointStart: Date.now(),
-                    pointInterval: 1000*5
+                    pointInterval: 1666
                 }],
                 selectedGaugeIdx: null,
             },
