bf2ff6fb59c6dcfd098e91c1b5548ed67cc700f2
Priyath Gregory <priyath.gregory@syscolabs.com>
2020-06-07 14:18:38 +0530

    round energy consumption in summary view

--- src/logic/branchSummaryManager.js
+++ src/logic/branchSummaryManager.js
@@ -2,7 +2,7 @@ export const transformBranchSummaryData = (data) => {
     let branchSummaryMap = {};
     return data.map(el => {
         return branchSummaryMap[el.location] = {
-            energyConsumption: el.totalEnergy,
+            energyConsumption: Math.round(el.totalEnergy),
             alertCount: el.alertCount,
             location: el.location,
         };
