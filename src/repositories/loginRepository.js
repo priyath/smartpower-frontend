0e4f5ba5f3051b241a7fdfb104a6e44caf97ffa2
Priyath Gregory <priyath.gregory@syscolabs.com>
2020-06-13 12:54:09 +0530

    temp fix: handle auth

--- src/repositories/loginRepository.js
+++ src/repositories/loginRepository.js
@@ -4,6 +4,14 @@ import {axiosFetchJSON} from './fetchAPI';
 export const authUser = ({username, password}) => {
     let formData = {username, password};
 
+    //TODO: remove this once auth API is deployed
+    return new Promise((resolve, reject) => {
+        setTimeout(() => resolve({data: {
+            username: 'admin',
+            accessToken: '3434j3kn43j4nk3j4erdr'
+        }}), 500);
+    })
+
     return axiosFetchJSON(AUTH_API_ENDPOINT + '/users/login', 'POST', formData)
         .then(response => response)
         .catch((e) => {
