fe25ec713dc418b040d212521fabef38095662c3
Priyath Gregory <priyath.gregory@syscolabs.com>
2020-06-13 14:06:26 +0530

    auth api endpoint added

--- src/repositories/APIConstants.js
+++ src/repositories/APIConstants.js
@@ -1,2 +1,2 @@
 export const REALTIME_API_ENDPOINT = 'http://3.91.188.96:80/api';
-export const AUTH_API_ENDPOINT = 'http://localhost:3005';
+export const AUTH_API_ENDPOINT = 'http://3.91.188.96/auth';
